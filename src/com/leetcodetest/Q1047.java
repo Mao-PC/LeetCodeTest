78145008f5c9eec67e48a247a74a83bae3f03e27
maopch <maopch@yonyou.com>
2020-08-26 22:55:11 +0800

    tij

---
+++ src/com/leetcodetest/Q1047.java
@@ -0,0 +1,43 @@
+package com.leetcodetest;
+
+import java.util.Stack;
+
+/**
+ * 删除字符串中的所有相邻重复项
+ */
+public class Q1047 {
+    static class Solution {
+        public String removeDuplicates(String S) {
+            if (S.length() == 1) return S;
+
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < S.length(); i++) {
+                char aChar = S.charAt(i);
+                if (stack.empty()) {
+                    stack.push(aChar);
+                } else {
+                    Character c = stack.peek();
+                    if (c.equals(aChar)) {
+                        stack.pop();
+                    } else {
+                        stack.push(aChar);
+                    }
+                }
+            }
+
+            char[] chars = new char[stack.size()];
+
+            int index = stack.size() - 1;
+            while (!stack.empty()) {
+                Character c = stack.pop();
+                chars[index--] = c;
+            }
+            return new String(chars);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = new Solution().removeDuplicates("abbaca");
+        System.out.println(s);
+    }
+}
---
+++ src/com/leetcodetest/Q682.java
@@ -0,0 +1,16 @@
+package com.leetcodetest;
+
+/**
+ * 棒球比赛
+ */
+public class Q682 {
+    class Solution {
+        public int calPoints(String[] ops) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
